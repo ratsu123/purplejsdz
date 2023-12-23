006163d2dacef108d5b6df77f51d723cc55e069e
Ratmir Suleyev <shikirmik2002@gmail.com>
2023-12-23 15:04:15 +0600

    map

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
 'use strict'
+const arr = [
+    {id:1, name:'Вася'},
+    {id:1, name:'Вася'},
+    {id:2, name:'Петр'},
+]
+function uni(arr) {
+    console.log(Array.from(
+        new Set(arr.map(n => n.name)),
+        n => arr.find(m => m.name === n)
+      ))
+    console.log(new Set(arr.map(n => n.name)))
+
+}
+uni(arr)
+
