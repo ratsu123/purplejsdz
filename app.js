2f0b637ba9e39444e1cc8edb1844329332309356
Ratmir Suleyev <shikirmik2002@gmail.com>
2023-12-23 16:23:37 +0600

    birthday

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
 'use strict'
+function olderThan14(birthday) {
+    const birthDate = new Date(birthday)
+
+    const birthDateObj = new Date(birthDate);
+
+    const currentDate = new Date();
+
+    const age = currentDate.getFullYear() - birthDateObj.getFullYear();
+
+    // Проверяем, старше ли 14 лет
+    return age > 14;
+
+}
+console.log(olderThan14('2002-10-30'))
